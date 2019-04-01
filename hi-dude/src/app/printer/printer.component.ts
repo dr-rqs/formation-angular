fb05c5e15142c6ea98fc2d0b63d881292412a4e5
dr.rqs <roques.antoine@gmail.com>
2019-04-01 10:16:30 +0200

    vol_4 : property binding

--- hi-dude/src/app/printer/printer.component.ts
+++ hi-dude/src/app/printer/printer.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PrinterComponent implements OnInit {
 
   printerName =  'Best printer';
+  isPrinterOn = true;
 
   constructor() { }
 

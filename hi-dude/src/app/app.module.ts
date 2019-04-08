57978906030eb947e1c4b556ba67805bdb4eace9
dr.rqs <roques.antoine@gmail.com>
2019-04-08 11:27:01 +0200

    vol_5 : *ngFor

--- hi-dude/src/app/app.module.ts
+++ hi-dude/src/app/app.module.ts
@@ -3,18 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SpeakerComponent } from './speaker/speaker.component';
-import { PrinterComponent } from './printer/printer.component';
 import {FormsModule} from "@angular/forms";
-import { TechnicalCardComponent } from './technical-card/technical-card.component';
 import { FootballClubComponent } from './football-club/football-club.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpeakerComponent,
-    PrinterComponent,
-    TechnicalCardComponent,
     FootballClubComponent
   ],
   imports: [
